845077a18aa6e48027d9f0a1f79e913911c3cd80
denos <costelasdenis@gmail.com>
2023-04-17 16:05:11 +0300

    Firebase voodoo magic for messages

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,17 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 import './screens/chat_screen.dart';
 
-void main() {
+void main() async {
+  //Loads the API key for firebase
+  await dotenv.load(fileName: ".env");
+  //Makes it so SystemChrome.setPreferredOrientations works
+  WidgetsFlutterBinding.ensureInitialized();
+  //Sets preffered orientations
+  SystemChrome.setPreferredOrientations([DeviceOrientation.portraitUp]);
+  //Runs the app on boot
   runApp(const MyApp());
 }
 
--- lib/screens/chat_screen.dart
+++ lib/screens/chat_screen.dart
@@ -1,4 +1,6 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 
 class ChatScreen extends StatelessWidget {
   const ChatScreen({super.key});
@@ -12,6 +14,21 @@ class ChatScreen extends StatelessWidget {
           child: Text('WorkS !'),
         ),
         itemCount: 10,
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          await Firebase.initializeApp();
+          // snapshots emits new values every time the data changes
+          FirebaseFirestore.instance
+              .collection('chats/1fZCnzqmNoOfXhXwTHLp/messages')
+              .snapshots()
+              .listen((event) {
+            event.docs.forEach((document) {
+              print(document['text']);
+            });
+          });
+        },
+        child: const Icon(Icons.add),
       ),
     );
   }
