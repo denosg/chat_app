8cad5b271cb4a282184c8779f617ab925f3c7a64
denos <costelasdenis@gmail.com>
2023-04-21 22:31:50 +0300

    added auth validators

--- lib/widgets/auth/auth_form.dart
+++ lib/widgets/auth/auth_form.dart
@@ -8,6 +8,25 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
+  final _formKey = GlobalKey<FormState>();
+
+  bool _isLogin = true;
+
+  String _userEmail = '';
+  String _username = '';
+  String _userPassword = '';
+
+  void _trySubmit() {
+    final isValid = _formKey.currentState!.validate();
+    FocusScope.of(context).unfocus();
+
+    if (isValid) {
+      _formKey.currentState!.save();
+
+      // use the values to send our auth request to firebase
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Center(
@@ -18,29 +37,66 @@ class _AuthFormState extends State<AuthForm> {
           child: Padding(
             padding: const EdgeInsets.all(16),
             child: Form(
+              key: _formKey,
               child: Column(
                 mainAxisSize: MainAxisSize.min,
                 children: [
                   TextFormField(
+                    key: ValueKey('e-mail'),
+                    validator: (value) {
+                      if (value == null || !value.contains('@')) {
+                        return 'Please enter a valid e-mail address';
+                      }
+                      return null;
+                    },
                     keyboardType: TextInputType.emailAddress,
-                    decoration: InputDecoration(labelText: 'e-mail'),
+                    decoration: const InputDecoration(labelText: 'e-mail'),
+                    onSaved: (value) {
+                      _userEmail = value!;
+                    },
                   ),
+                  if (!_isLogin)
+                    TextFormField(
+                      key: ValueKey('username'),
+                      validator: (value) {
+                        if (value == null || value.length < 4) {
+                          'Please enter at least 4 characters';
+                        }
+                        return null;
+                      },
+                      decoration: const InputDecoration(labelText: 'username'),
+                      onSaved: (value) {
+                        _username = value!;
+                      },
+                    ),
                   TextFormField(
-                    decoration: InputDecoration(labelText: 'username'),
-                  ),
-                  TextFormField(
+                    key: ValueKey('password'),
+                    validator: (value) {
+                      if (value == null || value.length < 6) {
+                        return 'Password must be at least 6 characters long.';
+                      }
+                    },
                     decoration: InputDecoration(labelText: 'password'),
                     obscureText: true,
+                    onSaved: (value) {
+                      _userPassword = value!;
+                    },
                   ),
                   const SizedBox(height: 12),
                   ElevatedButton(
-                    onPressed: () {},
-                    child: Text('login'),
+                    onPressed: _trySubmit,
+                    child: Text(_isLogin ? 'login' : 'signup'),
                   ),
                   TextButton(
-                    onPressed: () {},
+                    onPressed: () {
+                      setState(() {
+                        _isLogin = !_isLogin;
+                      });
+                    },
                     child: Text(
-                      'Create new account',
+                      _isLogin
+                          ? 'Create new account'
+                          : 'I already have an account',
                       style: TextStyle(
                           color: Theme.of(context).colorScheme.secondary),
                     ),
